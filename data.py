4cd71c77588530bedaeee9ae6ab400d7bb747a7e
tamikazi <tahmidkazi829@gmail.com>
2023-06-18 16:58:57 -0600

    rename, remove mvp

--- data.py
+++ data.py
@@ -11,14 +11,13 @@ for y in years:
 season_from = 2003
 season_to = 2004
 
-pl_frames = {}
+pl_pergame = {}
 
 for i in player_pergame:
-    pl_frames["pl" + str(season_from + 1)] = pd.read_html(player_pergame[i])[0]
-    pl_frames["pl" + str(season_from + 1)]['Season'] = str(season_from)+'-'+str(season_to)
-    pl_frames["pl" + str(season_from + 1)]['MVP'] = False
+    pl_pergame["pl" + str(season_from + 1)] = pd.read_html(player_pergame[i])[0]
+    pl_pergame["pl" + str(season_from + 1)]['Season'] = str(season_from)+'-'+str(season_to)
 
     season_from += 1
     season_to += 1
 
-print(pl_frames)
+print(pl_pergame)
